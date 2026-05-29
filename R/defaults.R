79c2699b02dcdb46632a1b910043aea90591e18d
Anjani Nandan <anjaninandan599@gmail.com>
2026-05-29 16:59:43 +0000

    implementing downloads and cmd check

---
+++ R/defaults.R
@@ -0,0 +1,30 @@
+mlr3hf_hub_url <- function(){
+    getOption("mlr3hf.hub_url",
+    Sys.getenv("MLR3HF_HUB_URL", "https://huggingface.co"))
+}
+mlr3hf_cache_dir <- function(){
+    getOption("mlr3hf.cache_dir",
+    Sys.getenv("MLR3HF_CACHE_DIR", rappdirs::user_cache_dir("mlr3hf")))
+}
+
+mlr3hf_parquet<- function(){
+    getOption("mlr3hf.parquet", TRUE)
+}
+mlr3hf_parquet_url<- function(){
+    getOption("mlr3hf.parquet_url",
+    Sys.getenv("MLR3HF_PARQUET_URL", "https://huggingface.co/api/datasets"))
+}
+mlr3hf_token <- function() {
+  token <- getOption("mlr3hf.hf_token", NULL)
+  if (!is.null(token) && nzchar(token)) return(token)
+# sometimes users use HF_TOKEN and sometimes HUGGING_FACE_HUB_TOKEN, so we check both
+  token <- Sys.getenv("HF_TOKEN", unset = "")
+  if (nzchar(token)) return(token)
+
+  token <- Sys.getenv("HUGGING_FACE_HUB_TOKEN", unset = "")
+  if (nzchar(token)) return(token)
+  NULL
+}
+mlr3hf_retries <- function() {
+  getOption("mlr3hf.retries", 3L) 
+}
