e8a179ffe825c0a611871065f5050a822d3b6610
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-10 11:54:13 +0000

    adding test for download description

---
+++ tests/testthat/test-download_desc.R
@@ -0,0 +1,37 @@
+test_that("testing download_desc", {
+skip_on_cran()
+skip_if_offline()
+res1<-download_desc("scikit-learn/iris")
+res2<-download_desc("scikit-learn/iris")
+expect_equal(res1$downloads, res2$downloads)
+expect_equal(res1$description, res2$description)
+})
+
+test_that("download_desc fails on error", {
+  local_mocked_bindings(
+    GET = function(...) {
+      structure(
+        list(status_code = 404),
+        class = "response"
+      )
+    },
+    http_error = function(...) TRUE,   
+    status_code = function(...) 404,   
+    content = function(...) "Not Found", 
+    .package = "httr"
+  )
+  expect_error(
+    download_desc("scikit-learn/iris"),
+    "Failed to retrieve metadata"
+  )
+})
+
+test_that("download_desc handles non-existent repo gracefully", {
+skip_on_cran()
+skip_if_offline()
+  
+res1 <- download_desc("scikit-learn/iris", files_metadata = FALSE)
+res2 <- download_desc("scikit-learn/iris", files_metadata = TRUE)
+
+expect_false(identical(res1, res2))
+})
