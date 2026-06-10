5380fb686a67ff7f30153b8ef34dd69ed2cc55e4
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-10 11:54:56 +0000

    adding roxygen

--- R/download_desc.R
+++ R/download_desc.R
@@ -1,3 +1,14 @@
+#' @title download_desc
+#' @description it helps in downloading the description
+#' @param repo_id repo id of the dataset
+#' @param revision revision of the dataset, default is "main"
+#' @param files_metadata whether to include files metadata, default is FALSE
+#' @param ... additional arguments passed to httr::GET
+#'
+#' @return list containing description and downloads of the dataset
+#' @examples
+#' download_desc("scikit-learn/iris")
+#' @export
 download_desc<-function(repo_id, revision = "main",...,files_metadata=FALSE){
     base_url<- mlr3hf_parquet_url()
     url<-glue::glue("{base_url}/{repo_id}/revision/{revision}")
