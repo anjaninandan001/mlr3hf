6ac6eb30049ab81bc8958e9acce2c1520129bd1f
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-11 11:14:10 +0000

    adding some tests that can be used as user-interface later

--- tests/testthat/test-download_desc.R
+++ tests/testthat/test-download_desc.R
@@ -26,12 +26,31 @@ test_that("download_desc fails on error", {
   )
 })
 
+test_that("files_metadata TRUE and FALSE return different responses", {
+  skip_on_cran()
+  skip_if_offline()
+  
+  res1 <- download_desc("scikit-learn/iris", files_metadata = FALSE)
+  res2 <- download_desc("scikit-learn/iris", files_metadata = TRUE)
+  
+  expect_false(identical(res1, res2))
+})
+
 test_that("download_desc handles non-existent repo gracefully", {
+  skip_on_cran()
+  skip_if_offline()
+  
+  expect_error(
+    download_desc("scikit-learn/nonexistentrepo"),
+    "Failed to retrieve metadata"
+  )
+})
+
+test_that("download_desc returns expected fields", {
 skip_on_cran()
 skip_if_offline()
-  
-res1 <- download_desc("scikit-learn/iris", files_metadata = FALSE)
-res2 <- download_desc("scikit-learn/iris", files_metadata = TRUE)
-
-expect_false(identical(res1, res2))
+res1 <- download_desc("scikit-learn/iris")
+res2 <- download_desc("scikit-learn/iris")
+expect_equal(res1$usedStorage, 5309549) #this value may change if the dataset is updated
+expect_equal(res1$siblings,res2$siblings) # these features i.e usedStorage, description and siblings will be used in HFData.R
 })
