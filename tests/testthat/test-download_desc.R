2028a831f7d500de685ad8027fe72b15cc6407fd
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-13 08:45:25 +0000

    make change in variable name

--- tests/testthat/test-download_desc.R
+++ tests/testthat/test-download_desc.R
@@ -1,10 +1,10 @@
 test_that("testing download_desc", {
 skip_on_cran()
 skip_if_offline()
-iris.first.call <- download_desc("scikit-learn/iris")
-iris.second.call <- download_desc("scikit-learn/iris")
-expect_equal(iris.first.call$downloads, iris.second.call$downloads)
-expect_equal(iris.first.call$description, iris.second.call$description)
+iris.desc.initial <- download_desc("scikit-learn/iris")
+iris.desc.repeated <- download_desc("scikit-learn/iris")
+expect_equal(iris.desc.initial$downloads, iris.desc.repeated$downloads)
+expect_equal(iris.desc.repeated$description, iris.desc.repeated$description)
 })
 
 test_that("download_desc fails on error", {
@@ -30,10 +30,10 @@ test_that("files_metadata TRUE and FALSE return different responses", {
   skip_on_cran()
   skip_if_offline()
   
-  iris.first.call <- download_desc("scikit-learn/iris", files_metadata = FALSE)
-  iris.second.call <- download_desc("scikit-learn/iris", files_metadata = TRUE)
+  iris.desc.initial <- download_desc("scikit-learn/iris", files_metadata = FALSE)
+  iris.desc.repeated <- download_desc("scikit-learn/iris", files_metadata = TRUE)
   
-  expect_false(identical(iris.first.call, iris.second.call))
+  expect_false(identical(iris.desc.initial, iris.desc.repeated))
 })
 
 test_that("download_desc handles non-existent repo gracefully", {
@@ -49,8 +49,8 @@ test_that("download_desc handles non-existent repo gracefully", {
 test_that("download_desc returns expected fields", {
 skip_on_cran()
 skip_if_offline()
-iris.first.call <- download_desc("scikit-learn/iris")
-iris.second.call <- download_desc("scikit-learn/iris")
-expect_equal(iris.first.call$usedStorage, 5309549) #this value may change if the dataset is updated
-expect_equal(iris.first.call$siblings, iris.second.call$siblings) # these features i.e usedStorage, description and siblings will be used in HFData.R
+iris.desc.initial <- download_desc("scikit-learn/iris")
+iris.desc.repeated <- download_desc("scikit-learn/iris")
+expect_equal(iris.desc.initial$usedStorage, 5309549) #this value may change if the dataset is updated
+expect_equal(iris.desc.initial$siblings, iris.desc.repeated$siblings) # these features i.e usedStorage, description and siblings will be used in HFData.R
 })
