1d1294674939ca562b618a10c1d125f68830b62e
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-13 09:12:45 +0000

    formated using air

--- tests/testthat/test-download_desc.R
+++ tests/testthat/test-download_desc.R
@@ -1,10 +1,10 @@
 test_that("testing download_desc", {
-skip_on_cran()
-skip_if_offline()
-iris.desc.initial <- download_desc("scikit-learn/iris")
-iris.desc.repeated <- download_desc("scikit-learn/iris")
-expect_equal(iris.desc.initial$downloads, iris.desc.repeated$downloads)
-expect_equal(iris.desc.repeated$description, iris.desc.repeated$description)
+  skip_on_cran()
+  skip_if_offline()
+  iris.desc.initial <- download_desc("scikit-learn/iris")
+  iris.desc.repeated <- download_desc("scikit-learn/iris")
+  expect_equal(iris.desc.initial$downloads, iris.desc.repeated$downloads)
+  expect_equal(iris.desc.repeated$description, iris.desc.repeated$description)
 })
 
 test_that("download_desc fails on error", {
@@ -15,9 +15,9 @@ test_that("download_desc fails on error", {
         class = "response"
       )
     },
-    http_error = function(...) TRUE,   
-    status_code = function(...) 404,   
-    content = function(...) "Not Found", 
+    http_error = function(...) TRUE,
+    status_code = function(...) 404,
+    content = function(...) "Not Found",
     .package = "httr"
   )
   expect_error(
@@ -29,17 +29,23 @@ test_that("download_desc fails on error", {
 test_that("files_metadata TRUE and FALSE return different responses", {
   skip_on_cran()
   skip_if_offline()
-  
-  iris.desc.initial <- download_desc("scikit-learn/iris", files_metadata = FALSE)
-  iris.desc.repeated <- download_desc("scikit-learn/iris", files_metadata = TRUE)
-  
+
+  iris.desc.initial <- download_desc(
+    "scikit-learn/iris",
+    files_metadata = FALSE
+  )
+  iris.desc.repeated <- download_desc(
+    "scikit-learn/iris",
+    files_metadata = TRUE
+  )
+
   expect_false(identical(iris.desc.initial, iris.desc.repeated))
 })
 
 test_that("download_desc handles non-existent repo gracefully", {
   skip_on_cran()
   skip_if_offline()
-  
+
   expect_error(
     download_desc("scikit-learn/nonexistentrepo"),
     "Failed to retrieve metadata"
@@ -47,10 +53,10 @@ test_that("download_desc handles non-existent repo gracefully", {
 })
 
 test_that("download_desc returns expected fields", {
-skip_on_cran()
-skip_if_offline()
-iris.desc.initial <- download_desc("scikit-learn/iris")
-iris.desc.repeated <- download_desc("scikit-learn/iris")
-expect_equal(iris.desc.initial$usedStorage, 5309549) #this value may change if the dataset is updated
-expect_equal(iris.desc.initial$siblings, iris.desc.repeated$siblings) # these features i.e usedStorage, description and siblings will be used in HFData.R
+  skip_on_cran()
+  skip_if_offline()
+  iris.desc.initial <- download_desc("scikit-learn/iris")
+  iris.desc.repeated <- download_desc("scikit-learn/iris")
+  expect_equal(iris.desc.initial$usedStorage, 5309549) #this value may change if the dataset is updated
+  expect_equal(iris.desc.initial$siblings, iris.desc.repeated$siblings) # these features i.e usedStorage, description and siblings will be used in HFData.R
 })
