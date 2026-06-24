4526bdcf2e11961e65d0a659b986f5075101c162
Anjani Nandan <anjaninandan599@gmail.com>
2026-06-24 09:23:39 +0000

    adding test case for list_datasets()

---
+++ tests/testthat/test-list_datasets.R
@@ -0,0 +1,18 @@
+test_that("list_dataset returns an error when <1 is given as input", {
+    skip_on_ci()
+    skip_if_offline()
+
+    expect_error(
+        list_datasets(-1),
+        "num_of_dataset should be positive"
+    )
+})
+test_that("list_dataset column types are as expected", {
+    skip_on_ci()
+    skip_if_offline()
+
+    datasets <- list_datasets(num_of_dataset = 5)
+    expect_type(datasets$id, "character")
+    expect_type(datasets$gated, "character")
+    expect_type(datasets$downloads, "integer")
+})
